d19be339d11fe5f8ba40bbe121e49e3dfe8361ab
taro <teroro1226@outlook.jp>
2023-05-26 13:54:02 +0900

    feat: divided and managed functions

---
+++ src/components/app.rs
@@ -0,0 +1,21 @@
+use yew::prelude::*;
+pub struct App;
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+    
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                <div class={"wrapper"}>
+                    <h1>{"Hello, World!"}</h1>
+                </div>
+            </>
+        }
+    }
+}
