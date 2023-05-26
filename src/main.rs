c1ec76e3f508c085e029a2f57ed033bed20916eb
taro <teroro1226@outlook.jp>
2023-05-26 13:26:43 +0900

    chore: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use yew::prelude::*;
+
+mod components;
+
+enum Msg {
+    AddOne,
+    ReduceOne,
+}
+
+struct App {
+    value: i64,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => { 
+                self.value += 1;
+                true
+            },
+            Msg::ReduceOne => { 
+                self.value -= 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <button onclick={ctx.link().callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <button onclick={ctx.link().callback(|_| Msg::ReduceOne)}>{ "-1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
